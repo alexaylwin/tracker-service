c506404e0aa2454ecebe7c010b8979931f895f11
Alex <AlexAylwin@gmail.com>
2016-11-03 11:58:24 -0400

    Updated stub data

--- api/recentActivities/RecentActivitiesGetHandler.class.php
+++ api/recentActivities/RecentActivitiesGetHandler.class.php
@@ -19,35 +19,35 @@ class RecentActivitiesGetHandler {
 		if($request[0] == '1') {
 			$response->setStatusHeader(200);
 			$responseText = <<<EOT
-	{startTime: 2016-03-11 10:47AM,
-		endTime: 2016-03-11 12:47PM,
-		duration: 120,
-		activityId: 2 },
-	{startTime: 2016-03-05 10:47AM,
-		endTime: 2016-03-05 11:47AM,
-		duration: 1900,
-		activityId: 3 },
-	{startTime: 2016-03-01 1:47PM,
-		endTime: 2016-03-01 2:47PM,
-		duration: 200,
-		activityId: 4 }
+	[{"startTime": "2016-03-11 10:47AM",
+		"endTime": "2016-03-11 12:47PM",
+		"duration": "120",
+		"activityId": "2" },
+	{"startTime": "2016-03-05 10:47AM",
+		"endTime": "2016-03-05 11:47AM",
+		"duration": "1900",
+		"activityId": "3"},
+	{"startTime": "2016-03-01 1:47PM",
+		"endTime": "2016-03-01 2:47PM",
+		"duration": "200",
+		"activityId": "4" }]
 EOT;
 			$response->setBody($responseText);
 		} else {
 			$response->setStatusHeader(200);
 			$responseText = <<<EOT
-	{startTime: 2019-03-11 10:47AM,
-		endTime: 2019-03-11 12:47PM,
-		duration: 120,
-		activityId: 1 },
-	{startTime: 2019-03-05 10:47AM,
-		endTime: 2019-03-05 11:47AM,
-		duration: 1900,
-		activityId: 1 },
-	{startTime: 2019-03-01 1:47PM,
-		endTime: 2019-03-01 2:47PM,
-		duration: 200,
-		activityId: 1 }
+[{"startTime": "2019-03-11 10:47AM",
+		"endTime": "2019-03-11 12:47PM",
+		"duration": "120",
+		"activityId": "1" },
+	{"startTime": "2019-03-05 10:47AM",
+		"endTime": "2019-03-05 11:47AM",
+		"duration": "1900",
+		"activityId": "1" },
+	{"startTime":" 2019-03-01 1:47PM",
+		"endTime": "2019-03-01 2:47PM",
+		"duration": "200",
+		"activityId": "1" }]
 EOT;
 			$response->setBody($responseText);
 		}
