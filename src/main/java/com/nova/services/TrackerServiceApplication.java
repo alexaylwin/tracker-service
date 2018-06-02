e40c1961321634b71f38c9e639d3bb36fd80540c
Alex <alex.aylwin@rbc.com>
2018-06-02 07:11:57 -0400

    Added spring security

--- src/main/java/com/nova/services/TrackerServiceApplication.java
+++ src/main/java/com/nova/services/TrackerServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class TrackerServiceApplication {
 
 	public static void main(String[] args) {
