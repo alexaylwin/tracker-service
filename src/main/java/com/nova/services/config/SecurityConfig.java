575e7a9f6b5a3790dad71d0ac45792b1afabe263
Alex <AlexAylwin@gmail.com>
2018-06-14 13:08:24 -0400

    Added basic auth security using spring security

--- src/main/java/com/nova/services/config/SecurityConfig.java
+++ src/main/java/com/nova/services/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication()
 			.withUser(credentials.getUsername())
 				.password(credentials.getPassword())
-				.roles("USER");
+				.roles("USER")
+			.and()
+			.passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	@Override
