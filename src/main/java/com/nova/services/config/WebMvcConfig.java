8e3006cd5caf3125b49b8c5189e051bbbbd442c7
Alex <AlexAylwin@gmail.com>
2018-06-14 13:09:35 -0400

    Removing unused code

--- src/main/java/com/nova/services/config/WebMvcConfig.java
+++ src/main/java/com/nova/services/config/WebMvcConfig.java
@@ -1,18 +1,11 @@
 package com.nova.services.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.nova.services.SecurityHandlerInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-	
-	//@Autowired
-	SecurityHandlerInterceptor securityInterceptor;
 	
 	@Override
 	  public void addCorsMappings(CorsRegistry registry) {
@@ -23,9 +16,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	    .allowedMethods("GET", "POST", "OPTIONS");
 	  }
 	
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		//registry.addInterceptor(securityInterceptor);
-	}
-
 }
